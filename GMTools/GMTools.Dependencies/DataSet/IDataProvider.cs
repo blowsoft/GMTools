9c18785f564e0d51ff8d2ee39e7cdfd33b7c4d8c
Mattia Bruno <blowsoftware@gmail.com>
2015-12-16 22:57:23 +0100

    US #2 Task #6

---
+++ GMTools/GMTools.Dependencies/DataSet/IDataProvider.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GMTools.Dependencies.DataSet
+{
+    /// <summary>
+    /// Interface for a piece of software which interconnects the main application with a provider of entities of any kind.
+    /// </summary>
+    public interface IDataProvider
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets the entities defined in this data-set.
+        /// </summary>
+        /// <value>
+        /// The entities defined in this data-set.
+        /// </value>
+        List<Type> Entities { get; }
+
+        #endregion
+
+        /// <summary>
+        /// Gets the repository for the specified entity type.
+        /// </summary>
+        /// <param name="entityType">Type of the entity.</param>
+        /// <returns></returns>
+        dynamic Repository(Type entityType);
+    }
+}
